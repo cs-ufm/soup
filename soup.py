48a428a2e6fd9928b04bef0ac9b4d30805196a6c
@jmarcos-cano <jmarcos.cano@gmail.com>
2019-09-10 08:22:50 -0600

    @jmarcos-cano - adds find_all(divs)

--- soup.py
+++ soup.py
@@ -19,6 +19,6 @@ soup = BeautifulSoup(html_content, "html.parser")
 print(soup.title)
 print(soup.title.string)
 
-for div in soup.find_all("div"):
+for div in soup.find_all("img"):
     print(div)
     print("--------------------------")
