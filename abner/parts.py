633063345986ec387fbbe40372118b4741e7cf16
abnerxch <axocop@ufm.edu>
2019-09-20 22:51:34 -0600

    Definiendo las partes en las que se divide el proyecto

---
+++ abner/parts.py
@@ -0,0 +1,21 @@
+class Parts:
+
+
+    def part_one(self):
+        return print("one")
+
+    def part_two(self):
+        return print("two")
+
+    def part_three(self):
+        return print("three")
+
+    def part_four(self):
+        return print("four")
+
+    partes = {
+        1 == part_one(),
+        2 == part_two(),
+        3 == part_three(),
+        4 == part_four()
+    }
