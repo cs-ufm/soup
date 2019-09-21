812bd527d487d0c4bbf58406f75e836334442ad9
abnerxch <axocop@ufm.edu>
2019-09-20 21:50:58 -0600

    Definiendo las funciones para la parte 1 (portal)

--- abner/portal.py
+++ abner/portal.py
@@ -12,12 +12,43 @@ except:
 # Parse the html content, this is the Magic ;)
 soup = BeautifulSoup(html_content, "html.parser")
 
+def get_title():
+    return print(f"GET the title and print it: {soup.title.string} ")
+
+def get_address():
+    return print("dir")
+
+def get_phone():
+    return print("phone")
+
+def get_email():
+    return print("email")
+
+def get_nav_menu():
+    return print("nav")
+
+def find_href():
+    return print("href")
+
+def get_href_UFMbutton():
+    return print("UFMbutton")
+
+def get_href_MIU():
+    return ("MiU")
+
+def get_href_img():
+    return ("img")
+
+def count_a():
+    return ("a")
+
 MY_NAME = "Abner"
-print(f"|| MY name is {MY_NAME} ||")
+print("="*60)
+print(f"\t\t|| MY name is {MY_NAME} ||")
 print("="*60)
 
 print("1. PORTAL")
+get_title()
+print("-"*60)
 
-print(f"GET the title and print it: {soup.title.string} ")
-#print(soup.title)
-#print(soup.title.string)
+print(f"")
